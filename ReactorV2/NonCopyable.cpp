b1dcb52b6d55b8369adbf4765abbb3eb7e908831
Peano_Taylor <Peano.Taylor@gmail.com>
2025-07-29 21:24:11 +0800

    NonCopyable impl

--- ReactorV2/NonCopyable.cpp
+++ ReactorV2/NonCopyable.cpp
@@ -2,9 +2,16 @@
  * Project 66th
  */
 
-
 #include "NonCopyable.h"
+class NonCopyable
+{
+public:
+    NonCopyable() = default;
+    ~NonCopyable() = default;
 
+    NonCopyable(const NonCopyable &rhs) = delete;
+    NonCopyable &operator=(const NonCopyable &rhs) = delete;
+};
 /**
  * NonCopyable implementation
  */
